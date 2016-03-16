55f155cdc4cea4c7c0b11e61303b5ec6a3e4b848
DickTse <dicktse1@gmail.com>
2016-03-16 08:30:59 +0800

    Initial project

---
+++ MultithreadSingleton/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultithreadSingleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thd1 = new Thread(new ThreadStart(runThread));
+            Thread thd2 = new Thread(new ThreadStart(runThread));
+            thd1.Start();
+            thd2.Start();
+            thd1.Join();
+            thd2.Join();
+            Console.Write("Program finished. Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        private static void runThread()
+        {
+            Singleton instance = Singleton.Instance;
+            instance.PrintMessage();
+        }
+    }
+}
---
+++ MultithreadSingleton/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultithreadSingleton
+{
+    class Singleton
+    {
+        private static object lck = new object();
+        private Singleton() { }
+        private static Singleton instance;
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Singleton();
+                return instance;
+            }
+        }
+        public void PrintMessage()
+        {
+            lock(lck)
+            { 
+                Console.WriteLine("Message 1");
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine("Message 2");
+            }
+        }
+    }
+}
