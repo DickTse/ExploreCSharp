6db7e6f2284c6df08afb75c763e4d4427bb7fa01
rohirrim <dicktse1@gmail.com>
2016-03-22 16:24:03 +0800

    Added project SingletonAndDerivedClass to test how a singleton class can be inherited.

---
+++ SingletonAndDerivedClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExploreCSharp.SingletonAndDerivedClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingletonBase baseObj = SingletonBase.Instance;
+            baseObj.StaticNum = 1;
+            SingletonChild childObj = SingletonChild.Instance;
+            childObj.StaticNum = 2;
+            Console.WriteLine(String.Format("baseObj: {0}", baseObj.StaticNum));
+            Console.WriteLine(String.Format("childObj: {0}", childObj.StaticNum));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SingletonAndDerivedClass/Singleton.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExploreCSharp.SingletonAndDerivedClass
+{
+    class SingletonBase
+    {
+        protected SingletonBase() { }
+        private static SingletonBase instance;
+        private int staticNum;
+        public static SingletonBase Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new SingletonBase();
+                return instance;
+            }
+        }
+        public int StaticNum
+        {
+            get { return staticNum; }
+            set { staticNum = value; }
+        }
+    }
+
+    class SingletonChild : SingletonBase
+    {
+        private SingletonChild() { }
+        private static SingletonChild instance;
+        public new static SingletonChild Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new SingletonChild();
+                return instance;
+            }
+        }
+    }
+}
