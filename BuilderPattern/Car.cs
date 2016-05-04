35e3f61f198305f32be773f4a767310e224315a1
dicktse <dicktse1@gmail.com>
2016-05-04 23:22:41 +0800

    Added the following projects: - BuilderPattern -> Examine the BuilderPattern discussed in GoF's book. - List -> to examine the duration and memory it takes to convert a List<implemented class> objects to its List<interface> objects.

---
+++ BuilderPattern/Car.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPattern {
+    // Represents a product created by the builder.
+    class Car {
+        public Car() { }
+        public int Wheels { get; set; }
+        public string Color { get; set; }
+        public override string ToString() {
+            return String.Format("Color: {0}; Wheels {1}", Color, Convert.ToString(Wheels));
+        }
+    }
+
+    // The builder abstraction.
+    interface ICarBuilder {
+        void SetColor(string color);
+        void SetWheels(int count);
+        Car GetResult();
+    }
+
+    // Concrete builder implementation.
+    class CarBuilder : ICarBuilder {
+        private Car _car;
+        public CarBuilder() {
+            this._car = new Car();
+        }
+        public void SetColor(string color) {
+            this._car.Color = color;
+        }
+        public void SetWheels(int count) {
+            this._car.Wheels = count;
+        }
+        public Car GetResult() {
+            return this._car;
+        }
+    }
+
+    // The director
+    class CarBuilderDirector {
+        public Car Construct() {
+            CarBuilder builder = new CarBuilder();
+            builder.SetColor("Red");
+            builder.SetWheels(4);
+            return builder.GetResult();
+        }
+    }
+}
---
+++ List/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace List {
+    interface IClass1 {
+        string Str { get; set; }
+    }
+
+    class Class2 : IClass1 {
+        public string Str { get; set; }
+    }
+    class Program {
+        private const int MAX_CYCLE = 10;
+        static void Main(string[] args) {
+            TimeSpan totalTime = new TimeSpan();
+            for (int j=0; j<MAX_CYCLE; j++) { 
+                Stopwatch sw = new Stopwatch();
+                List<Class2> strings = new List<Class2>();
+                sw.Start();
+                for (int i=0; i<1000000; i++) {
+                    strings.Add(new Class2() { Str = new String('A', 1024) });
+                }
+                List<IClass1> class1 = strings.ConvertAll(x => (IClass1)x);
+                sw.Stop();
+                totalTime += sw.Elapsed;
+                Console.WriteLine($"Elapsed time of round {j+1}: {sw.Elapsed}.");
+            }
+            Console.WriteLine($"Total elapsed time = {totalTime}");
+            Console.WriteLine($"Average elapsed time = {totalTime.TotalSeconds / MAX_CYCLE}s");
+            Console.Read();
+        }
+    }
+}
