29a891f73c7228a67935a2d0ec60f75cd63ee68e
rohirrim <dicktse1@gmail.com>
2016-05-05 09:52:17 +0800

    Add projects InnerExcpetion and RegularExpression

---
+++ InnerException/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InnerException
+{
+    class Program
+    {
+        private static void writeInnerExceptionToConsole(Exception ex)
+        {
+            if (ex.InnerException != null)
+            {
+                Console.WriteLine("Inner-exception message:" + ex.InnerException.Message);
+                Console.WriteLine("Inner-exception Stack Trace:");
+                Console.WriteLine(ex.InnerException.StackTrace);
+                writeInnerExceptionToConsole(ex.InnerException);
+            }
+        }
+
+        private static void innerMethodWithException2()
+        {
+            throw new Exception("Exception thrown in innerMethodWithException2()");
+        }
+
+        private static void innerMethodWithException()
+        {
+            try
+            {
+                innerMethodWithException2();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Exception thrown in innerMethodWithException()", ex);
+            }
+        }
+
+        private static void methodWithException()
+        {
+            try
+            {
+                innerMethodWithException();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Exception thrown in methodWithException()", ex);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calling methodWithException()...");
+            try
+            {
+                methodWithException();
+                Console.WriteLine("Method is executed successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception is encounterd when calling methodWithException().");
+                Console.WriteLine("Exception message:" + ex.Message);
+                Console.WriteLine("Stack Trace:");
+                Console.WriteLine(ex.StackTrace);
+                writeInnerExceptionToConsole(ex);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RegularExpression/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace RegularExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = @"ABCD!@#$%^&*()_+~{}|:""<>?`-=[]\; ',./a";
+            Console.WriteLine(str);
+            Match match = Regex.Match(str, @"[A-Z0-9a-z!@#$%^&*()_+~{}|:""<>?`\-=\[\]\\; ',./]+");
+            //Match match = Regex.Match(str, @"$\d+");
+            if (match.Success)
+            {
+                string outstr = match.Groups[0].Value;
+                Console.WriteLine(outstr);
+            }
+            Console.Read();
+        }
+    }
+}
